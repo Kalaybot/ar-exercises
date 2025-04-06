2f975ece35471998c3c5e658ce2bb496a34ac856
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-04-06 17:54:25 -0400

    Calculate total revenue af all stores, output stores that make over 1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+# Total revenue of all stores
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue of the entire company: $#{@total_revenue}"
+
+# Average annual revenue of all stores
+@average_revenue = Store.average(:annual_revenue)
+puts "Average Annual Revenue of all stores: $#{@average_revenue}"
+
+# Number of stores generating $1M or more in annual sales
+@stores_over_1M = Store.where("annual_revenue >= ?", 1000000).size
+puts "Number of stores generating $1M or more in annual sales: #{@stores_over_1M}"
