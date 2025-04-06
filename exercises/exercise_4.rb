4b5726d401806c989b5d6739e645dcd4ca742ec5
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-04-06 17:39:03 -0400

    Load subset stores, use where method to fetch subsets of store depending on condition

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,26 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# New store creation
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# Output the number of stores using count method
+puts "Number of stores: #{Store.count}"
+
+# Fetch stores with mens apparel, assign instance variable
+@mens_stores = Store.where(mens_apparel: true)
+
+# Loop through each mens store and output name and annual revenue
+@mens_stores.each do |store|
+  puts "Store Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
+
+# Fetch stores with womens apparel and annual revenue less than 1000000
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+
+# Loop through each womens store and output name and annual revenue
+@womens_stores.each do |store|
+  puts "Store Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
