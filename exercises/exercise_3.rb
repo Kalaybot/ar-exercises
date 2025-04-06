6407f83e0c784508c82c1dce35a5faf977a71d51
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-04-06 17:27:54 -0400

    Delete store 3, use destroy method to delete

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+# Instance variables for store 3
+@store3 = Store.find(3)
+
+# Delete store 3 from the database
+@store3.destroy
+
+# Output remaining stores
+puts " Number of stores: #{Store.count}"
