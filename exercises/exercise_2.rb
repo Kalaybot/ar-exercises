26abd7e0576286bf6a66f420ef19dcbc4369af84
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-04-06 17:24:26 -0400

    Update the first store, assign instance variables

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Assign the stores to a variable
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+# Update the first store
+@store1.update(name: "Burnaby 2.0")
+puts @store1.name
